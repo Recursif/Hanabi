231532cb0027d22fe27d9c549b8dcf0333059cfb
Recursif <jillian.dum@gmail.com>
2018-08-18 19:07:07 +0200

    add get action and print instruction play

--- hanabi.py
+++ hanabi.py
@@ -63,6 +63,38 @@ def print_board():
     print(second_line)
     print("")
 
+def print_actions():
+    print("Choississez une action parmi celles-ci:")
+    print("")
+    print("1- Donner un indice")
+    print("2- Déffausser une carte")
+    print("3- Piocher")
+    print("4- Quittez")
+    print("")
+
+def print_instruction_play(hand):
+    print("Choississez une carte que vous voulez jouer")
+    print("")
+    for i in range(5):
+        print(str(i) + "- " + hand[i])
+    print("")
+
+
+
+def get_action():
+    action_value = input("")
+    print("")
+    while (not((action_value) in ["1","2","3","4"])):
+        print_board()
+        print("attention la valeur choisie doit être entre 1 et 4")
+        print("")
+        action_value = input("")
+        print("")
+    
+    return (action_value)
+        
+
+
 
 # numéro du joueur actuel: [0 : nombre de joueurs - 1]
 turn = 0
@@ -79,6 +111,28 @@ print("\n*************\n")
 
 print_players_hand()
 print_board()
+print_actions()
+
+action = get_action()
+
+if (action == "1"):
+    print_instruction_play(hands_list[turn])
+    card_to_play = get_card_to_play()
+    if (is_valid_vard_to-play(board,card)):
+        play_card(card)
+    else:
+        print_error()
+        errors_number += 1
+elif (action == "2"):
+    print("Nope")
+elif(action == "3"):
+    print("Nope")
+else:
+    print("au revoir!!")
+    end_game = True
+
+
+#print(action)
 
 """
 while (not(end_game)):
