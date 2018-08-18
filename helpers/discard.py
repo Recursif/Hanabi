cfae9d793bb6c13e3fea7790d739c9705842f91f
Satijah <xxatak@gmail.com>
2018-08-19 00:10:32 +0200

    implementing printing function, simplification

--- helpers/discard.py
+++ helpers/discard.py
@@ -1,8 +1,3 @@
-table = []
-error = 0
-token = 0
-
-
 colors = [
     "B",
     "Y",
@@ -11,12 +6,25 @@ colors = [
     "G",
 ]
 
+
+# --- Discard Instructions ---
+
+def print_instruction_discard(know_infos): 
+    print("Choississez la carte à défausser :")
+    print("")
+    for i in range(5):
+        print(str(i + 1) + "- " + know_infos[i])
+    print("")
+
+
+# --- Retrieving card to discard ---
+
 def get_card_to_discard(hand):
     action_value = input("")
     print("")
     while (not((action_value) in ["1","2","3","4","5"])):
         print_board()
-        print("attention la valeur choisie doit être entre 1 et 5")
+        print("Attention la valeur choisie doit être entre 1 et 5")
         print("")
         action_value = input("")
         print("")
@@ -25,33 +33,28 @@ def get_card_to_discard(hand):
     return (card_to_discard)
 
 
+
+# --- Discard Function ---
+
+
 def discard():
     print_instruction_discard()
     card = get_card_to_discard()
-    
 
+    # Retrieving card location
+    card_num = int(card_to_discard[0]) -1
+    card_color = card_to_discard[1]
+    card_color = colors.index(card_color)
 
-#table generation
-for i in range (5):
-    table.append([3,2,2,1,1])
+    # Tokens resolving
+    if table[card_color][card_num]==1: 
+        error_token += 1    
+    else:
+        clue_token += 1
+    # Removing card from the table
+    table[card_color][card_num] -= 1
 
-#récupération de l'emplacement de la carte
-card_num = int(card[0]) -1
-card_color = card[1]
-card_color = colors.index(card_color)
-
-#résolution des erreurs et tokens
-if table[card_color][card_num]==1: 
-    error += 1    
-else:
-    token += 1
-
-#carte retirée de la liste
-table[card_color][card_num] -= 1
-
-#si pile de couleur non finissable, carte de valeur supérieure ne compte pas comme des erreurs
-if table[card_color][card_num]==0:
-    for i in range(5-card_num):
-        table[card_color][card_num+i]=0
-
-#print (table)
+    # Particular case for unfinishable color of cards
+    if table[card_color][card_num]==0:
+        for i in range(5-card_num):
+            table[card_color][card_num+i]=0
